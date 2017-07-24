ffb5c41a78c11b93c4ba49f536307741b6930e21
tangtantivirun <tang2tasketball@gmail.com>
2017-07-24 15:39:04 +0800

    Login View Controller.swif added

---
+++ FacePass/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  FacePass
+//
+//  Created by Tang Tantivirun on 7/24/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseAuthUI
+import FirebaseDatabase
+
+class LoginViewController {
+    
+}
