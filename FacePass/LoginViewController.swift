3398f9217394da511eaea60757d19599917d2b8c
tangtantivirun <tang2tasketball@gmail.com>
2017-07-24 22:08:13 +0800

    Created Firebase Login

--- FacePass/LoginViewController.swift
+++ FacePass/LoginViewController.swift
@@ -20,6 +20,33 @@ class LoginViewController : UIViewController {
     
     @IBAction func loginButtonTapped(_ sender: Any) {
     
+        guard let authUI = FUIAuth.defaultAuthUI() else { return }
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewControler, animated: true)
     }
-    
+}
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ _authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        if let error = error {
+            assertionFailure ("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        guard let user = user else { return }
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        userRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            if let user = User(snapshot: snapshot) {
+                
+                User.setCurrent(user, writeToUserDefaults: true)
+                
+                let initialViewController = UIStoryboard.initialViewController(for: .main)
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+            } else {
+                
+                self.performSegue(withIdentifier: "toCreateAccount", sender: self)
+            }
+        })
+    }
 }
