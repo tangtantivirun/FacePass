81ef9a6e8765fefca7d35ef4ff6ab4e22be5d313
tangtantivirun <tang2tasketball@gmail.com>
2017-07-24 16:34:55 +0800

    CreateUserViewController , User.swift,

--- FacePass/LoginViewController.swift
+++ FacePass/LoginViewController.swift
@@ -10,7 +10,16 @@ import UIKit
 import FirebaseAuth
 import FirebaseAuthUI
 import FirebaseDatabase
+typealias FIRUser = FirebaseAuth.User
 
-class LoginViewController {
+class LoginViewController : UIViewController {
+    @IBOutlet weak var loginButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButtonTapped(_ sender: Any) {
+    
+    }
     
 }
