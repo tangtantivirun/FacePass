f33581b5870b889b2fa53c3f4400f3c3309ef95c
tangtantivirun <tang2tasketball@gmail.com>
2017-07-26 11:13:01 +0800

    created Member class and member service

---
+++ FacePass/MemberService.swift
@@ -0,0 +1,15 @@
+//
+//  MemberService.swift
+//  FacePass
+//
+//  Created by Tang Tantivirun on 7/26/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+struct MemberService {
+    
+}
