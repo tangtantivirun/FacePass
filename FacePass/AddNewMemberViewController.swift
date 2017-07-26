10a8aa3389a3429eeb9b8945d3e9942c025713f4
Jenniferry0701 <yujia911210575@163.com>
2017-07-26 12:02:46 +0800

    added addMemberViewController

---
+++ FacePass/AddNewMemberViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddNewMemberViewController.swift
+//  FacePass
+//
+//  Created by Jennifer Gao on 26/7/2017.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import UIKit
+
+
+class AddNewMemberViewController: UIViewController
+{
+    
+}
