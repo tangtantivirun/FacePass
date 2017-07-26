64cb1e47029f278e95d7cd7ae15e32f28b297d99
Jenniferry0701 <yujia911210575@163.com>
2017-07-26 14:09:08 +0800

    completed add-member

--- FacePass/AddNewMemberViewController.swift
+++ FacePass/AddNewMemberViewController.swift
@@ -12,4 +12,21 @@ import UIKit
 class AddNewMemberViewController: UIViewController
 {
     
+    @IBOutlet weak var addMemberButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var ageTextField: UITextField!
+    @IBOutlet weak var genderSelector: UISegmentedControl!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func AddMemberButtonTapped(_ sender: Any) {
+    
+    
+    }
+    
 }
