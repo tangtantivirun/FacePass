7c151036844ed1969bd885c76d9385b154662165
tangtantivirun <tang2tasketball@gmail.com>
2017-07-27 09:50:44 +0800

    added code to member class

--- FacePass/Member.swift
+++ FacePass/Member.swift
@@ -1,14 +1,36 @@
-////
-////  Member.swift
-////  FacePass
-////
-////  Created by Tang Tantivirun on 7/26/17.
-////  Copyright © 2017 JT. All rights reserved.
-////
 //
-//import Foundation
+//  Member.swift
+//  FacePass
+//
+//  Created by Tang Tantivirun on 7/26/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
 //
 
-class Member {
-    // TODO
+import Foundation
+import FirebaseDatabase.FIRDataSnapshot
+
+
+class Member:NSObject {
+    let  name: String
+    let birthday: String
+    let gender: Bool
+    var email: String
+    var phone: String
+    let id: String
+    let key: String
+    
+    init(from snapshot: DataSnapshot) {
+        
+        let snapshotValue = snapshot.value as! [String: Any]
+        
+        self.name = snapshotValue["name"] as! String
+        self.birthday = snapshotValue["birthday"] as! String
+        self.gender = snapshotValue["gender"] as! Bool
+        self.email = snapshotValue["email"] as! String
+        self.phone = snapshotValue["phone"] as! String
+        self.id = snapshotValue["id"] as! String
+        self.key = snapshot.key
+    
+}
 }
