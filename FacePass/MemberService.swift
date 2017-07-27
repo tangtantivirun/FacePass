ba97c77c00140a969b085b2a57424b53d9eec922
tangtantivirun <tang2tasketball@gmail.com>
2017-07-27 13:16:13 +0800

    S3 Working!

--- FacePass/MemberService.swift
+++ FacePass/MemberService.swift
@@ -20,6 +20,10 @@ struct MemberService {
     // TODO add params for member name, occupation, age, etc.
     static func create(image: UIImage, name: String, birthday: String, gender: Bool, email: String, phone: String, id: String, completion: @escaping (Member?) ->Void){
         
+        guard let imageData =  UIImageJPEGRepresentation(image,0.8) else {
+            fatalError("Image to data conversion failure")
+        }
+        
         // get unique memberID
         //let reference  = Database.database().reference().child("members").child(User.current.uid).childByAutoId()
         //let memberID = reference.key
@@ -34,83 +38,22 @@ struct MemberService {
         
             //TODO
         
-        // Save image to documents on disk and get url
-        let diskImageURL = MemberService.save(image, toMember: id)
-        MemberService.uploadToS3(localImageURL: diskImageURL)
         
-        // Do completion
-        completion(nil)
-    }
-    
-    // Parameters: Image of new member's face
-    // Return value: URL of image saved on disk
-    private static func save(_ image: UIImage, toMember memberID: String) -> URL {
-        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
-        //let dir = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first!
-        let documentsDirectory = paths[0]
-        let destinationURL = documentsDirectory.appendingPathComponent(memberID).appendingPathComponent("image.jpeg")
-        //let destinationURLString = documentsDirectory.appendingPathComponent(memberID).appendingPathComponent("image.jpeg").absoluteString
-        guard let imageData =  UIImageJPEGRepresentation(image,0.8) else {
-            fatalError("Image to data conversion failure")
-        }
-//        do {
-//            try FileManager.default.createDirectory(at: destinationURL, withIntermediateDirectories: true)
-//            try imageData.write(to: destinationURL)
-//        } catch let error {
-//            print(error.localizedDescription)
-//        }
-        print (destinationURL)
         let transferUtility = AWSS3TransferUtility.default()
         let expression = AWSS3TransferUtilityUploadExpression()
         
         transferUtility.uploadData(
             imageData,
             bucket: "facepass-lasthope",
-            key: "image.jpeg",
+            key: "\(id)/image.jpeg",
             contentType: "image/jpeg",
-            expression: expression).continueOnSuccessWith(block: { task in
-                print("Success")
+            expression: expression).continueWith(block: { task in
+                if let error = task.error {
+                    print(error.localizedDescription)
+                    return completion(nil)
+                }
+                // Create new member and send back through completion handler
+                return nil
             })
-        return destinationURL
-}
-
-    private static func uploadToS3(localImageURL url: URL) {
-        
-//        let uploadRequest = AWSS3TransferManagerUploadRequest()
-//        uploadRequest?.bucket = "facepass-ios"
-//        //uploadRequest?.acl = .publicRead
-//        uploadRequest?.key = "image.jpeg"   //name of the file when uploaded
-//        //uploadRequest?.contentType = "image/jpeg"
-//        uploadRequest?.body = url
-//        print("we made it half way")
-//        
-//        if let test = try? uploadRequest?.validate() {
-//            
-//        } else {
-//            print("Invalid request")
-//        }
-//        
-//        let transferManager = AWSS3TransferManager.default()
-//        transferManager.upload(uploadRequest!).continueOnSuccessWith(block: { task in
-//            print("Success")
-//        })
-        
-        /*
-        transferManager.upload(uploadRequest!).continueWith(block: { (task: AWSTask<AnyObject>) -> Any? in
-            if let error = task.error {
-                print("Upload failed with error: (\(error.localizedDescription))")
-            }
-
-            if task.result != nil {
-                print("we did it!")
-                //                let url = AWSS3.default().configuration.endpoint.url
-                //                let publicURL = url?.appendingPathComponent((uploadRequest?.bucket!)!).appendingPathComponent((uploadRequest?.key!)!)
-                // print("Uploaded to:\(publicURL ?? <#default value#>)")
-            }
-            return nil
-        })
-        */
     }
-
-
 }
