350199b8bd06a7be579e9bdb60002db6589a92f7
Jenniferry0701 <yujia911210575@163.com>
2017-07-31 12:04:55 +0800

    started to create the list all member storyboard

--- FacePass/ListCurrentMemberViewController.swift
+++ FacePass/ListCurrentMemberViewController.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+import UIKit
+
+
+class ListCurrentMemberViewController: UIViewController
+{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        notes = CoreDataHelper.retrieveNote()!
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any? )
+    {
+        if let identifier = segue.identifier
+        {
+            if identifier == "displayNote"
+            {
+                let indexPath = tableView.indexPathForSelectedRow!
+                let note = notes[indexPath.row]
+                let displayNoteViewController = segue.destination as! DisplayNoteViewController
+                displayNoteViewController.note = note
+                print("Table view cell tapped")
+            }
+            else if identifier == "addNote"
+            {
+                print("+ button tapped")
+            }
+        }
+    }
+ 
+}
