52bcdef366a28135f4c5691ac908af72e4f61163
Jenniferry0701 <yujia911210575@163.com>
2017-08-11 16:18:03 +0800

    random

--- FacePass/DisplayInfoViewController.swift
+++ FacePass/DisplayInfoViewController.swift
@@ -13,4 +13,18 @@ import UIKit
 class DisplayInfoViewController: UIViewController
 {
     
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var membershipIDLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+
+
+    @IBOutlet weak var checkEnteringButtonTapped: UIButton!
+
+
+
 }
