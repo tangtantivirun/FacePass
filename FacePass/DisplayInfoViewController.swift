854ea0a0ba30cb42de988883348db896810655e8
Jenniferry0701 <yujia911210575@163.com>
2017-08-20 10:10:27 +0800

    display info of all members

--- FacePass/DisplayInfoViewController.swift
+++ FacePass/DisplayInfoViewController.swift
@@ -21,10 +21,34 @@ class DisplayInfoViewController: UIViewController
     @IBOutlet weak var phoneLabel: UILabel!
     @IBOutlet weak var membershipIDLabel: UILabel!
     @IBOutlet weak var button: UIButton!
-
-
     @IBOutlet weak var checkEnteringButtonTapped: UIButton!
+    var member: Member?
 
-
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if let member = member
+        {
+            nameLabel.text = member.name
+            birthdayLabel.text = member.birthday
+            genderLabel.text = member.gender.rawValue
+            emailLabel.text = member.email
+            phoneLabel.text = member.phone
+            membershipIDLabel.text = member.id
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let identifier = segue.identifier
+        {
+            if identifier == "cancel"
+            {
+                print("Cancel button tapped")
+            }
+        }
+    }
 }
--- FacePass/ListCurrentMemberViewController.swift
+++ FacePass/ListCurrentMemberViewController.swift
@@ -1,41 +1,47 @@
-////
-////  ListCurrentMemberViewController.swift
-////  FacePass
-////
-////  Created by Tang Tantivirun on 7/27/17.
-////  Copyright © 2017 JT. All rights reserved.
-////
 //
-//import Foundation
-//import UIKit
+//  ListCurrentMemberViewController.swift
+//  FacePass
 //
+//  Created by Tang Tantivirun on 7/27/17.
+//  Copyright © 2017 JT. All rights reserved.
 //
-//class ListCurrentMemberViewController: UIViewController
-//{
-//    
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
+
+import Foundation
+import UIKit
+
+
+class ListCurrentMemberViewController: UITableViewController
+{
+    var members = [Member]()
+    {
+        didSet
+        {
+            tableView.reloadData()    //what is tableView
+//            members.insert(notes[notes.count-1], at: 0)
+//            members.remove(at: notes.count-1)
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
 //        notes = CoreDataHelper.retrieveNote()!
-//    }
-//    
-//    
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any? )
-//    {
-//        if let identifier = segue.identifier
-//        {
-//            if identifier == "displayNote"
-//            {
-//                let indexPath = tableView.indexPathForSelectedRow!
-//                let note = notes[indexPath.row]
-//                let displayNoteViewController = segue.destination as! DisplayNoteViewController
-//                displayNoteViewController.note = note
-//                print("Table view cell tapped")
-//            }
-//            else if identifier == "addNote"
-//            {
-//                print("+ button tapped")
-//            }
-//        }
-//    }
-// 
-//}
+    }
+    
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any? )
+    {
+        if let identifier = segue.identifier
+        {
+            if identifier == "displayNote"
+            {
+                let indexPath = tableView.indexPathForSelectedRow!
+                let member = members[indexPath.row]
+                let displayInfoViewController = segue.destination as! DisplayInfoViewController
+                displayInfoViewController.member = member
+                print("Table view cell tapped")
+            }
+           
+        }
+    }
+ 
+}
