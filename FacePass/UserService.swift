71df44b006fc05bd8abdc16bf4295b048f55b53d
Jenniferry0701 <yujia911210575@163.com>
2017-08-26 12:13:25 +0800

    created faceset

--- FacePass/UserService.swift
+++ FacePass/UserService.swift
@@ -13,8 +13,28 @@ import FirebaseDatabase
 struct UserService {
 
 static func create(_ firUser: FIRUser, account: String, completion: @escaping (User?) ->Void){
-    let userAttrs = ["account": account]    
-    let ref = Database.database().reference().child("users").child(firUser.uid)
+    let userAttrs = ["company name": account]
+    let parameters = ["api-key":"XWUByShXgb6CConfOR5-T3ORi5CDsJAL",
+                      "api_secret": "P5cpB52PnrOBdIZ2jIJpKGco7c4W9Uom",
+                      "display_name": account] as [String : Any]
+    
+    Alamofire.request(apiToContact, method:.post, parameters: parameters, encoding: URLEncoding.default).responseJSON { response in
+        switch response.result {
+        case .success:
+            if let value = response.result.value {
+                let json = JSON(value)
+                
+                // Do what you need to with JSON here!
+                // The rest is all boiler plate code you'll use for API requests
+                
+                
+            }
+        case .failure(let error):
+            print(error)
+        }
+    }
+
+    let ref = Database.database().reference().child("users").child(firUser.email!)
     ref.setValue(userAttrs) { (error, ref) in
         if let error = error {
             assertionFailure(error.localizedDescription)
