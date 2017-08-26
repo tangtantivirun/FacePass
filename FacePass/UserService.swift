f4d9dc330ae4b2612d1d5433a38a0fde76e678d5
Jenniferry0701 <yujia911210575@163.com>
2017-08-26 16:39:27 +0800

    api

--- FacePass/UserService.swift
+++ FacePass/UserService.swift
@@ -14,39 +14,36 @@ import Alamofire
 struct UserService {
 
 static func create(_ firUser: FIRUser, account: String, completion: @escaping (User?) ->Void){
-    let userAttrs = ["company name": account]
-    let parameters = ["api-key":"XWUByShXgb6CConfOR5-T3ORi5CDsJAL",
-                      "api_secret": "P5cpB52PnrOBdIZ2jIJpKGco7c4W9Uom",
-                      "display_name": account] as [String : Any]
-    let apiToContact = "https://api-us.faceplusplus.com/facepp/v3/faceset/create"
-    Alamofire.request(apiToContact, method:.post, parameters: parameters, encoding: URLEncoding.default).responseJSON { response in
-        switch response.result {
-        case .success:
-            if let value = response.result.value {
-<<<<<<< HEAD
-=======
-              
-                
-                // Do what you need to with JSON here!
-                // The rest is all boiler plate code you'll use for API requests
-                
-                
-            }
-        case .failure(let error):
-            print(error)
-        }
-    }
-
+    let userAttrs = ["account": account]
     let ref = Database.database().reference().child("users").child(firUser.uid)
     ref.setValue(userAttrs) { (error, ref) in
         if let error = error {
             assertionFailure(error.localizedDescription)
             return completion(nil)
         }
+        
+        let parameters = ["api_key":"XWUByShXgb6CConfOR5-T3ORi5CDsJAL",
+                          "api_secret":"P5cpB52PnrOBdIZ2jIJpKGco7c4W9Uom",
+                          "display_name": account] as [String : Any]
+        let apiToContact = "https://api-us.faceplusplus.com/facepp/v3/faceset/create"
+        Alamofire.request(apiToContact, method:.post, parameters: parameters, encoding: URLEncoding.default).responseJSON { response in
+            switch response.result {
+            case .success:
+                if let value = response.result.value {
+
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+        
         ref.observeSingleEvent(of: .value, with: { (snapshot) in
             let user = User(snapshot: snapshot)
             completion(user)
+        
         })
+        
+
     }
 }
   
