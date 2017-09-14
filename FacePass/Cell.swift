feda2219696a5ad801c68a4c386ead423dc8ace2
tangtantivirun <tang2tasketball@gmail.com>
2017-09-13 23:06:52 -0400

    displaying member's name in the table view cells

---
+++ FacePass/Cell.swift
@@ -0,0 +1,15 @@
+//
+//  Cell.swift
+//  FacePass
+//
+//  Created by Tang Tantivirun on 9/13/17.
+//  Copyright © 2017 JT. All rights reserved.
+//
+
+import UIKit
+
+class Cell: NSObject {
+    
+    var name: String?
+   
+}
--- FacePass/ListCurrentMemberViewController.swift
+++ FacePass/ListCurrentMemberViewController.swift
@@ -6,58 +6,89 @@
 //  Copyright © 2017 JT. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import Firebase
-import FirebaseStorage
+import FirebaseDatabase
 
 
 class ListCurrentMemberViewController: UITableViewController
 {
-    var posts = [Member]()
-    var databaseRef: DatabaseReference!
-    var storageRef: StorageReference!
+    var ref: DatabaseReference!
+    var refHandle: UInt!
+    var memberList = [Cell]()
+    
+    let cellId = "cellId"
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        databaseRef = Database.database().reference()
-        storageRef = Storage.storage().reference()
-        
+        ref = Database.database().reference()
+        fetchMembers()
+
     }
-    
-     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
+    override func tableView(_ tableView: UITableView,numberOfRowsInSection section: Int) ->Int {
+       return memberList.count
     }
-    
-     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return posts.count
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
+       
+        //set cell contents (what goes inside the cells)
+        cell.textLabel?.text = memberList[indexPath.row].name
         
+        return cell
     }
-     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cellIdentifier = "postCell"
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)as! CustomTableViewCell
-        
-        let userPostRef = self.databaseRef.child("posts")
-        userPostRef.observe(.childAdded, with: {(snapshot) in
-            if let postAdd  = snapshot.value as? NSDictionary{
-                let myPost = Member(from: snapshot)
-                self.posts.insert(myPost!, at:0)
+    func fetchMembers(){
+        refHandle = ref.child("members").observe(.childAdded, with: { (snapshots) in
+            if let dictionary = snapshots.value as? [String: AnyObject] {
+                print(dictionary)
+               
+                let member = Cell()
                 
-                //Dispatch the main thread here
-                DispatchQueue.main.async() {
-                    cell.nameLabel.text = self.posts[indexPath.row].name
-                    
-                }
+                member.setValuesForKeys(dictionary)
+                self.memberList.append(member)
+               
+                DispatchQueue.main.async(execute: {
+                    self.tableView.reloadData()
+                })
             }
         })
-        return cell
-}
 
-    @IBAction func unwindToMainVC(_ sender: Any) {
-//            let mainVC = UIStoryboard.initialViewController(for: .main)
-//            self.view.window?.rootViewController = mainVC
-//            self.view.window?.makeKeyAndVisible()
     }
 }
+//    
+//     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 1
+//    }
+//    
+//     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        // #warning Incomplete implementation, return the number of rows
+//        return posts.count
+//        
+//    }
+//     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+//        let cellIdentifier = "postCell"
+//        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath as IndexPath)as! CustomTableViewCell
+//        
+//        let userPostRef = self.databaseRef.child("posts")
+//        userPostRef.observe(.childAdded, with: {(snapshot) in
+//            if let postAdd  = snapshot.value as? NSDictionary{
+//                let myPost = Member(from: snapshot)
+//                self.posts.insert(myPost!, at:0)
+//                
+//                //Dispatch the main thread here
+//                DispatchQueue.main.async() {
+//                    cell.nameLabel.text = self.posts[indexPath.row].name
+//                    
+//                }
+//            }
+//        })
+//        return cell
+//}
+//
+//    @IBAction func unwindToMainVC(_ sender: Any) {
+////            let mainVC = UIStoryboard.initialViewController(for: .main)
+////            self.view.window?.rootViewController = mainVC
+////            self.view.window?.makeKeyAndVisible()
+//    }
+//}
